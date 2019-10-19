e029776a0dd110541a1c7eef4d9283f3529558a8
Bill Griffin <williamgriffin81@hotmail.com>
2019-10-19 01:37:56 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -21,6 +21,8 @@ end
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
+	programmer_hash[]
+	
 	
 	programmer_hash = 
  		{
