54795a6b08405433c8fae2097582e97ac5bd7c6b
Bill Griffin <williamgriffin81@hotmail.com>
2019-10-19 01:47:29 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -66,8 +66,8 @@ def adding_matz
 #   :known_for => "Ruby",
 #    :languages => ["LISP", "C"]
 # }
-programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
-programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
+#programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+#programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
 
 	programmer_hash = 
  		{
